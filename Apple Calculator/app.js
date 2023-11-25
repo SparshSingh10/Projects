a9d70fa0893db565110cf0ed49d4d7debfe01474
SparshSingh10 <sparsh262002@gmail.com>
2023-11-25 18:57:27 +0530

    new folder upload

---
+++ Apple Calculator/app.js
@@ -0,0 +1,20 @@
+let inputE=document.querySelector('input');
+let btns=document.querySelectorAll('button')
+
+for(let button of btns){
+    button.addEventListener('click',function(e){
+        let btnText=e.target.innerText;
+        if(btnText=='AC')
+            inputE.value="";
+        else if(btnText==='='){
+            try{
+
+                inputE.value=eval(inputE.value);
+            }
+            catch(err){
+                inputE.value='invalid';
+            }}
+        else
+        inputE.value=inputE.value+btnText;
+    })
+}
